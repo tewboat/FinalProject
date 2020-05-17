8004b55bd6097dbaee8dd67264a5ef9a3d7992f1
Ilya Zhdanov <zdravulin@gmail.com>
2020-05-17 14:30:56 +0500

    completed app with some bugs(DB bug, menu bug)

--- app/src/main/java/com/example/finalproject/repository/notification/NotificationHelper.java
+++ app/src/main/java/com/example/finalproject/repository/notification/NotificationHelper.java
@@ -1,14 +1,17 @@
 package com.example.finalproject.repository.notification;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
 import androidx.core.app.NotificationCompat;
 
 import com.example.finalproject.R;
+import com.example.finalproject.view.MainActivity;
 
 import static com.example.finalproject.App.NOTIFICATION_CHANNEL_ID;
 
@@ -28,11 +31,16 @@ public class NotificationHelper extends ContextWrapper {
     }
     public NotificationCompat.Builder getChannelNotification(String title) {
         Bitmap icon = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.ic_action_name);
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                0, activityIntent, 0);
         return new NotificationCompat.Builder(getApplicationContext(), NOTIFICATION_CHANNEL_ID)
                 .setSmallIcon(R.mipmap.app_icon_round)
                 .setLargeIcon(icon)
                 .setContentTitle(title)
-                .setContentText("Have you done it?");
+                .setContentText("Have you done it?")
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true);
     }
 
 
